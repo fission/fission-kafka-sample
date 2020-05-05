19a994055705a178bb15b226c4adbb70c718644c
Vishal <vishal-biyani@users.noreply.github.com>
2020-05-05 12:30:51 +0530

    Added visualization of all messages (#10)

--- 02_iot_data_consumer/src/main/java/io/fission/api/IotApiStats.java
+++ 02_iot_data_consumer/src/main/java/io/fission/api/IotApiStats.java
@@ -1,10 +1,15 @@
 package io.fission.api;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.logging.Logger;
 
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import io.fission.Context;
 import io.fission.Function;
@@ -12,17 +17,23 @@ import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
 
-public class IotFlushData implements Function  {
+public class IotApiStats implements Function  {
 	
 	private static Logger logger = Logger.getGlobal();
+	final ObjectMapper mapper = new ObjectMapper();
 	JedisPool pool = new JedisPool(new JedisPoolConfig(), "redis-single-master.redis");
-
 
 	public ResponseEntity call(RequestEntity req, Context context) {
 		Jedis jedis = null;
+		HashMap<String,String> statCount = new HashMap<String,String>();
+		String data = "";
 		try {
 			jedis = pool.getResource();
-			jedis.flushDB();
+			statCount.put("TOTAL_MSG_SENT", jedis.hget("RECORD_SENT_BY_PRODUCER", "COUNT"));
+			statCount.put("TOTAL_MSG_PROCESSED", jedis.hget("RECORD_ACK_BY_CONSUMER", "COUNT"));
+			data = mapper.writeValueAsString(statCount);
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
 		} finally {
 			// You have to close jedis object. If you don't close then
 			// it doesn't release back to pool and you can't get a new
@@ -31,10 +42,11 @@ public class IotFlushData implements Function  {
 				jedis.close();
 			}
 		}
+		
 		HttpHeaders headers = new HttpHeaders();
 		headers.add(HttpHeaders.CONTENT_TYPE, "application/json; charset=UTF-8");
 		headers = IotApiFuel.AddCorsHeaders(headers);
-		return ResponseEntity.ok().headers(headers).build();
+		return ResponseEntity.ok().headers(headers).body(data);
 	}
 
 }
